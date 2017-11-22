184d218b501c16aaf69ec5b45cbf4f4b06441f4f
eliranm <eliranm@liveperson.com>
2017-11-22 17:49:57 +0200

    npm scope it (due name collision)

--- bin/release.sh
+++ bin/release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm publish && git push && echo -e "\n\n- - - don't forget to tag the release on github! - - -\n\n"
+npm publish --access=public && git push && echo -e "\n\n- - - don't forget to tag the release on github! - - -\n\n"
